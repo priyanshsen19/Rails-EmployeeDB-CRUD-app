94e06b88ca68dc3d29f4888705ab317cf17208c9
priyanshsen19 <priyanshsen21thjune@gmail.com>
2022-12-22 06:45:23 +0530

    final commit

--- db/migrate/20221219062509_create_firms.rb
+++ db/migrate/20221219062509_create_firms.rb
@@ -1,8 +1,8 @@
 class CreateFirms < ActiveRecord::Migration[7.0]
   def change
     create_table :firms do |t|
-      t.string :techskill_id
       t.string :employee_id
+      t.string :techskill_id
       t.string :name
       t.string :verified
 
